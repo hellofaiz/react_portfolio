3b1e0502b7176997438cdcf8b322539cbc78a30c
Faiz khan <98544944+hellofaiz@users.noreply.github.com>
2022-10-21 19:47:29 +0530

    Update Contact.js

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -6,15 +6,15 @@ function Contact() {
       <h2>📞 Contact</h2><br />
       <ul>
         <li>
-          <a href="https://www.linkedin.com/in/hello-faiz/" target="_blank">LinkedIn</a>
+          <a href="https://www.linkedin.com/in/hello-faiz/"rel="noreferrer" target="_blank">LinkedIn</a>
         </li>
 
         <li>
-          <a href="https://github.com/hellofaiz" target="_blank">GitHub</a>
+          <a href="https://github.com/hellofaiz"rel="noreferrer" target="_blank">GitHub</a>
         </li>
 
         <li>
-          <a href="https://faizkhan-f.vercel.app/" target="_blank">Website</a>
+          <a href="https://faizkhan-f.vercel.app/"rel="noreferrer" target="_blank">Website</a>
         </li>
 
       </ul>
